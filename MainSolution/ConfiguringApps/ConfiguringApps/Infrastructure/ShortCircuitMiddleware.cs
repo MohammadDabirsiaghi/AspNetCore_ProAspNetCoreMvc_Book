7d77d6efbe5154f6f48a1b967d6c86aa836e0699
Administrator <Administrator@USER-PC>
2018-05-14 22:58:48 +0430

    ShortCircuitMiddleware

---
+++ MainSolution/ConfiguringApps/ConfiguringApps/Infrastructure/ShortCircuitMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfiguringApps.Infrastructure
+{
+    public class ShortCircuitMiddleware
+    {
+        private RequestDelegate nextDelegare;
+        public ShortCircuitMiddleware(RequestDelegate next)
+        {
+            nextDelegare = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Headers["User-Agent"].Any(h => h.ToLower().Contains("edge")))
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+            else
+              await  nextDelegare.Invoke(httpContext);
+
+        }
+    }
+}
