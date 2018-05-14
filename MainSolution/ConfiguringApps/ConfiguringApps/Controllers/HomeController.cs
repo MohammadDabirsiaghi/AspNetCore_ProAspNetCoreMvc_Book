837ad0239e1566ab27371d2552e32fe267db7e38
Administrator <Administrator@USER-PC>
2018-05-14 22:20:46 +0430

    First Init

---
+++ MainSolution/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService _uptime;
+        public HomeController(UptimeService uptime)
+        {
+            this._uptime = uptime;
+
+        }
+        public IActionResult Index()
+        {
+            var data = new Dictionary<string, string>
+            {
+                ["Message"] = "This Is Test Message",
+                ["Uptime"] = $"{_uptime.Uptime}ms"
+            };
+            
+            return View(data);
+        }
+    }
+}
---
+++ MainSolution/ConfiguringApps/ConfiguringApps/Infrastructure/ContentMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfiguringApps.Infrastructure
+{
+    public class ContentMiddleware
+    {
+        private RequestDelegate nextDelegate;
+        public ContentMiddleware(RequestDelegate next)
+        {
+            nextDelegate = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.ToString().ToLower() == "/middleware")
+            {
+                await httpContext.Response.WriteAsync("This Is From Content Middleware", Encoding.UTF8);
+            }
+            else
+                await nextDelegate.Invoke(httpContext);
+        }
+    }
+}
